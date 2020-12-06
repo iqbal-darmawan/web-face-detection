56c6ad99ff44e07828d5daaf3fa50db8105599ea
IQBAL DARMAWAN <iqbaldarmawan24@gmail.com>
2020-12-06 20:24:50 +0700

    filebaru

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function animate(){
+  document.querySelector('.img-btn').addEventListener('click',function()
+    {
+      document.querySelector('.container').classlist.toggle('s-signup')
+    }
   );
+}
+class App extends Component {
+ 
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="form sign-in">
+            <h2>Sign In</h2>
+            <label htmlFor="">
+              <span>Email address</span>
+              <input type="email" name="email"/>
+            </label>
+            <label htmlFor="">
+              <span>Password</span>
+              <input type="password" name="password"/>
+            </label>
+            <button className="submit" type="button">Sign In</button>
+            <p className="forgot-pass">Forgot password</p>
+          </div>
+
+          <div className="sub-container">
+            <div className="img">
+              <div className="img-text m-up">
+                <h2>New Here?</h2>
+                <p>Sign up and discover great ammount of new  opportunities</p>
+              </div>
+              <div className="img-text m-in">
+                <h2>One of us?</h2>
+                <p>if you alredy has an account, just sign in. We've missed you!</p>
+              </div>
+              <div className="img-btn" onClick={animate()}>
+                <span className="m-up">Sign Up</span>
+                <span className="m-in">Sign In</span>
+              </div>
+            </div>
+            <div className="form sign-up">
+              <h2>Sign Up</h2>
+              <label htmlFor="">
+                <span>Name</span>
+                <input type="text"/>
+              </label>
+              <label htmlFor="">
+                <span>Email</span>
+                <input type="email"/>
+              </label>
+              <label htmlFor="">
+                <span>Passwoord</span>
+                <input type="password"/>
+              </label>
+              <label htmlFor="">
+                <span>Confirm Password</span>
+                <input type="password"/>
+              </label>
+              <button type="button" className="submit">Sign Up Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
